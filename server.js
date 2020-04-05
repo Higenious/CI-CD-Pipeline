9a92a055688091eef5bf4e9bb19553036a977665
Chetan tayade <cchetantayade@gmail.com>
2020-04-05 23:06:27 +0530

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+ var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+
+app.get('/', function (req, res) {
+ res.send('hello world');
+});
+//listen to port 3000 by default
+app.listen(3000, ()=>{
+ console.log('started successfully !');
+});
+ 
+module.exports = app;
